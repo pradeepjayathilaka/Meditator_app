52e547d35c30ee566be19c954d5c37c089b9ce7e
pradeepjayathilaka <pradeepjayathilaka06@gmail.com>
2025-01-12 06:41:20 +0530

    Add YouTube video player to function page and enhance UI details

--- lib/pages/function_page.dart
+++ lib/pages/function_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meditator_app/models/function_data_model.dart';
+import 'package:meditator_app/utils/colors.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class FunctionPage extends StatefulWidget {
   FunctionData functionData;
@@ -13,12 +15,90 @@ class FunctionPage extends StatefulWidget {
 }
 
 class _FunctionPageState extends State<FunctionPage> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    //get video id from url
+    final videoId = YoutubePlayer.convertUrlToId(widget.functionData.url) ?? "";
+
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Function"),
+        title: Text(
+          widget.functionData.title,
+        ),
       ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (_controller.initialVideoId.isNotEmpty)
+                AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: YoutubePlayer(controller: _controller),
+                  ),
+                )
+              else
+                const Text("No video available!"),
+              const SizedBox(
+                height: 40,
+              ),
+              Text(
+                "Duration: ${widget.functionData.duration} min",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: AppColors.primaryGrey,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Category: ${widget.functionData.category} ",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: AppColors.primaryGreen,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                widget.functionData.description,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  color: AppColors.primaryDarkBlue,
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
